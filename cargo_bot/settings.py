1aea8ccf7ae600985f2f369b383cdd52ad25a418
Nodirjon Ro'zimurodov <82306944+Nodirjon2303@users.noreply.github.com>
2021-11-22 11:38:51 +0500

    ready

--- cargo_bot/settings.py
+++ cargo_bot/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-7-6ry3)z^cdwh@@)-)8@54$y*gd9w%=@5txxs+uq&-jidvb3k7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
